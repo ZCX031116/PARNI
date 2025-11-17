11384ca6727770c2fbf7b8b6d9a576ee944f2d26
CZ <122865502+ZCX031116@users.noreply.github.com>
2025-11-16 19:37:57 -0500

    Add single-step PARNI proposal interface

--- algorithms_python/__init__.py
+++ algorithms_python/__init__.py
@@ -3,5 +3,22 @@ from .adr import adr
 from .parni import parni
 from .logit import logit_eps, inv_logit_eps
 from .dag_utils import is_dag_adjmat, h_to_permi_pars
+from .parni_step import (
+    ParniContext,
+    parni_prepare_context,
+    parni_make_LA_from_G,
+    parni_propose_one_step,
+)
 
-__all__ = ["adr", "parni", "logit_eps", "inv_logit_eps", "is_dag_adjmat", "h_to_permi_pars"]
+__all__ = [
+    "adr",
+    "parni",
+    "logit_eps",
+    "inv_logit_eps",
+    "is_dag_adjmat",
+    "h_to_permi_pars",
+    "ParniContext",
+    "parni_prepare_context",
+    "parni_make_LA_from_G",
+    "parni_propose_one_step",
+]
